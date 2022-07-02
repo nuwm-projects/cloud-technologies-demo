7a6ba96d8d7fbcbf26397a939ada8f6224a29710
Maksym Mishchanchuk <maximilanus0404@gmail.com>
2022-07-02 14:40:29 +0300

    Apdate up

--- app/backend/src/app/Api/Controllers/CounterController.php
+++ app/backend/src/app/Api/Controllers/CounterController.php
@@ -22,7 +22,7 @@ class CounterController extends BaseController
         }
 
         return response()->json([
-            'count' => $record->count,
+            'count' => $record->count * 10,
         ]);
     }
 
